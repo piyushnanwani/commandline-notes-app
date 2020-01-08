c7c4cf798d221c3b589fee451cba79cb4e46a474
piyush.nanwani <pnanwani61@gmail.com>
2020-01-08 11:08:19 +0530

    Commandline based notes app : First commit

---
+++ notebook.py
@@ -0,0 +1,53 @@
+import datetime
+
+# Store the next available id for all new notes
+last_id = 0
+
+class Note:
+    """ Represent a note in the notebook. Match 
+    against a string in searches
+    and store tags for each note."""
+
+    def __init__(self,memo, tags=''):
+        ''' initialize a note with memo and optional space-seperated tags. 
+        Automatically set the note's creation date and a unique id. '''
+        self.memo = memo 
+        self.tags = tags 
+        self.creation_date = datetime.date.today()
+        global last_id 
+        last_id +=1 
+        self.id = last_id
+
+    def match(self, filter):
+        '''Determine if this note matches the filter text.
+        Return True if it matches , False otherwise
+        Search is case sensitive and matches both text and tags
+        '''
+        return filter in self.memo or filter in self.tags 
+
+class Notebook:
+    ''' Represent a collection of notes that can be tagged, 
+        modified and searched.'''
+
+    def __init__(self):
+        ''' Initialise a notebook witha an empty list '''
+        self.notes = []
+
+    def new_note(self, memo, tags=''):
+        ''' Create a note and add it to the list.'''
+        self.notes.append(Note(memo,tags))
+    
+    def modify_tags(self, note_id, tags=''):
+        ''' Find the note with the given id and change it's memo to the given 
+        value '''
+        for note in notes:
+            if note.id == note_id:
+                note.tags = tags
+                break 
+        
+    def search(self, filter):
+        ''' Find all the notes that match the given filter string. '''
+        return [note for note in self.notes if note.match(filter)] 
+
+        
+
